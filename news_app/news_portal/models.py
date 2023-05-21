b59f8828404370fcb321b8c2b9de93efc16e260c
D-level <anna.kozina@gmail.com>
2023-05-21 21:45:12 +0300

    initial commit

---
+++ news_app/news_portal/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    author_rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        all_post_rating = Post.objects.filter(author_id=self.pk).aggregate(post_rating=Sum('post_rating'))['post_rating'] * 3
+        author_comment_rating = Comment.objects.filter(user_id=self.author).aggregate(comment_rating=Sum('comment_rating'))['comment_rating']
+        post_comment_rating = Comment.objects.filter(post__author__author=self.author).aggregate(comment_rating=Sum('comment_rating'))['comment_rating']
+        self.author_rating = all_post_rating + author_comment_rating + post_comment_rating
+        self.save()
+        return self.author_rating
+
+
+class Category(models.Model):
+    name_category = models.CharField(max_length=124, unique=True)
+
+
+article = 'A'
+news = 'N'
+KIND = [
+    (article, 'Article'),
+    (news, 'News')
+]
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_kind = models.CharField(max_length=1, choices=KIND, default=news)
+    time_in = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    post_rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.post_rating += 1
+        self.save()
+
+    def dislike(self):
+        self.post_rating -= 1
+        self.save()
+
+    def preview(self):
+        return f'{self.content[0:124]}...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    time_in = models.DateTimeField(auto_now_add=True)
+    comment_rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.comment_rating += 1
+        self.save()
+
+    def dislike(self):
+        self.comment_rating -= 1
+        self.save()
+
