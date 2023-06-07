9d049097492e97d88796c36c824a55713d54d759
D-level <anna.kozina@gmail.com>
2023-06-07 21:39:46 +0300

    commit

---
+++ news_app/news_portal/templatetags/custom_filters.py
@@ -0,0 +1,18 @@
+from django import template
+
+
+register = template.Library()
+
+
+censor_filter = ['Fuck', 'Bullshit', 'Dick', 'Редиска']
+
+
+@register.filter()
+def censor(word):
+    if isinstance(word, str):
+        for a in word.split():
+            if a.capitalize() in censor_filter:
+                word = word.replace(a, a[0] + '*' * len(a))
+    else:
+        raise ValueError('censored')
+    return word
